22c9e5c09b5363bf0bdd0ca4326afc1af3e4f9e9
ThomasZani <thomas79pro@gmail.com>
2017-04-11 10:36:53 +0200

    Aggiunto progetto XMLPersone

---
+++ Zani.Thomas.5i.XMLPersone/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+
+namespace Zani.Thomas._5i.XMLPersone
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            XElement dati = XElement.Load("Persone.xml");
+            IEnumerable<XElement> tuttepersone = dati.Elements("Persona");
+
+            ObservableCollection<Persona> Lista = new ObservableCollection<Persona>();
+
+            foreach(var ogg in tuttepersone)
+            {
+                Lista.Add(new Persona(ogg));
+            }
+
+            DAtti.ItemsSource = Lista;
+        }
+
+        
+
+    }
+}
---
+++ Zani.Thomas.5i.XMLPersone/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Zani.Thomas._5i.XMLPersone
+{
+    class Persona
+    {
+        public string Nome {get;set;}
+        public string Cognome { get; set;}
+        public string Indirizzo { get; set; }
+        public string Numero { get; set; }
+
+        public Persona (XElement e)
+        {
+            Nome = e.Attribute("nome").Value;
+            Cognome = e.Attribute("cognome").Value;
+            Indirizzo = e.Attribute("indirizzo").Value;
+            Numero = e.Attribute("numero").Value;
+        }
+    }
+}
